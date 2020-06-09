ba2eb27f2cdaa2cc161955ab20f644b6ae835aeb
Kushal Dutta <kushaldutta1749@gmail.com>
2020-06-08 19:48:12 -0700

    added new activity

--- app/src/main/java/com/example/rubikscubetutorial/MainActivity.java
+++ app/src/main/java/com/example/rubikscubetutorial/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.rubikscubetutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBackground();
+
+            }
+        });
+
     }
+public void openBackground() {
+        Intent intent = new Intent(this, Background.class);
+        startActivity(intent);
+}
 }
