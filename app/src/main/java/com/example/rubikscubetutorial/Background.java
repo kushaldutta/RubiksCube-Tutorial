67fc01ca14222dc3d4fff9127ba123f364635a24
Kushal Dutta <kushaldutta1749@gmail.com>
2020-06-10 13:21:09 -0700

    made buttons lead to main activity

--- app/src/main/java/com/example/rubikscubetutorial/Background.java
+++ app/src/main/java/com/example/rubikscubetutorial/Background.java
@@ -2,13 +2,42 @@ package com.example.rubikscubetutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Background extends AppCompatActivity {
+public class Background extends AppCompatActivity implements View.OnClickListener {
 
+    private Button next1;
+    private Button back1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_background);
+
+        back1 = (Button) (findViewById(R.id.back1));
+        next1 = (Button) (findViewById(R.id.next1));
+        back1.setOnClickListener(this);
+        next1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.back1:
+                openMainActivity();
+                break;
+            case R.id.next1:
+                openMainActivity();
+                break;
+        }
+    }
+
+    private void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
+
--- app/src/main/java/com/example/rubikscubetutorial/MainActivity.java
+++ app/src/main/java/com/example/rubikscubetutorial/MainActivity.java
@@ -8,15 +8,15 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openBackground();
