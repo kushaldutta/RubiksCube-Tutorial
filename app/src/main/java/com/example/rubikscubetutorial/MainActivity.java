1ca4f860b56612def3d61721361cf80dd070cec0
Kushal Dutta <kushaldutta1749@gmail.com>
2020-06-24 08:52:01 -0700

    created notation activity

--- app/src/main/java/com/example/rubikscubetutorial/MainActivity.java
+++ app/src/main/java/com/example/rubikscubetutorial/MainActivity.java
@@ -7,26 +7,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    private Button start;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button start = findViewById(R.id.start);
+        Button notation = findViewById(R.id.notation);
 
-        start = (Button) findViewById(R.id.start);
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openBackground();
-
-            }
-        });
+        start.setOnClickListener(this);
+        notation.setOnClickListener(this);
 
     }
-public void openBackground() {
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start:
+                openBackground();
+                break;
+            case R.id.notation:
+                openNotation();
+                break;
+        }
+    }
+
+    private void openBackground() {
         Intent intent = new Intent(this, Background.class);
         startActivity(intent);
-}
+    }
+
+    private void openNotation() {
+        Intent intent = new Intent(this, Notation.class);
+        startActivity(intent);
+    }
 }
