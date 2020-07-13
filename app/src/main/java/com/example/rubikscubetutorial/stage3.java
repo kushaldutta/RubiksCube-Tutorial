13c841c5613784296a24e23bb9afb689660df3b9
Kushal Dutta <kushaldutta1749@gmail.com>
2020-07-12 18:00:21 -0700

    created stage

---
+++ app/src/main/java/com/example/rubikscubetutorial/stage3.java
@@ -0,0 +1,14 @@
+package com.example.rubikscubetutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class stage3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stage3);
+    }
+}
